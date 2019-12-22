e0248db7f02043b4b1ab382e82f60cf97ce10827
Anze <anze@grafolean.com>
2019-12-22 13:53:41 +0100

    Use parent components (PersistentFetcher)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const RemoteComponent = props => {
-  return <div>Hello Remote {props.name ? props.name : '...whatever it is...'}!</div>;
-};
 
-export default RemoteComponent;
+export default class MyWidget extends React.Component {
+  state = {
+    accountName: null,
+  }
+
+  onAccountUpdate = json => {
+    this.setState({
+      accountName: json.name,
+    })
+  }
+
+  render() {
+    const { g: { components: { PersistentFetcher } } } = this.props;
+    const { accountName } = this.state;
+
+    return (
+      <div>
+        <PersistentFetcher resource="accounts/1" onUpdate={this.onAccountUpdate} />
+        {accountName === null ? "Loading..." : `Hello ${accountName}` }
+      </div>
+    )
+  }
+}

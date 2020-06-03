4445c1e354cffeff119c8acd21f41adb2556a35b
Anze <anze@grafolean.com>
2020-06-03 22:49:50 +0200

    Add prettier

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-import React from "react";
+import React from 'react';
 
 import './index.scss';
 
 export default class MyWidget extends React.Component {
   state = {
     accountName: null,
-  }
+  };
 
-  onAccountUpdate = json => {
+  onAccountUpdate = (json) => {
     this.setState({
       accountName: json.name,
-    })
-  }
+    });
+  };
 
   render() {
-    const { g: {
-      components: { PersistentFetcher },
-      urlParams: { accountId },
-    } } = this.props;
+    const {
+      g: {
+        components: { PersistentFetcher },
+        urlParams: { accountId },
+      },
+    } = this.props;
     const { accountName } = this.state;
 
     return (
       <div className="plugin-my-widget">
         <PersistentFetcher resource={`accounts/${accountId}`} onUpdate={this.onAccountUpdate} />
-        {accountName === null ? "Loading..." : `Hello ${accountName}` }
+        {accountName === null ? 'Loading...' : `Hello ${accountName}`}
       </div>
-    )
+    );
   }
 }
